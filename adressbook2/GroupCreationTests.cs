70ad2fbbf6c8cb8e6c35aa785e306bbad730dc5d
Evgenii Emelianov <evgeniy-95@bk.ru>
2024-04-03 00:23:02 +0300

    реструкторизация кода путем создания вспомогательных методов

---
+++ adressbook2/GroupCreationTests.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AdressBook_web_test
+{
+    [TestFixture]
+    public class GroupCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/addressbook/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void GroupCreationTest()
+        {
+            OpenHomePage();
+            LogOn("admin", "secret");
+            GoToGroupPage();
+            InitGroupCreation();
+            FillGroupForm("NameGroupTest","Header Group test","footer group test");
+            SubmitGroupCreation();
+            ReturnGroupPage();
+            LogOut();
+        }
+
+        private void LogOut()
+        {
+            //Logout
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturnGroupPage()
+        {
+            //return to group page
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            //Submit group creation
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(string name,string header, string footer)
+        {
+            //Fill group form
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            //Init new group creation
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupPage()
+        {
+            //go to group page
+            driver.FindElement(By.Id("header")).Click();
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void LogOn(string username, string password)
+        {
+            
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ adressbook2/UnitTest.cs
@@ -0,0 +1,15 @@
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdressBook_web_test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
